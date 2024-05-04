426018ecc7e3e5f11d04eeeb3feb1d97afefdf92
sl16 <vojtabar119@gmail.com>
2024-05-04 11:01:11 +0200

    cpp01 ex04 progress

--- cpp01/ex04/src/main.cpp
+++ cpp01/ex04/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vbartos <vbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 17:19:55 by vbartos           #+#    #+#             */
-/*   Updated: 2024/05/03 17:49:38 by vbartos          ###   ########.fr       */
+/*   Updated: 2024/05/04 11:00:23 by vbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,23 @@
 
 int	inputParser(int argc, char **argv)
 {
-	std::fstream file;
-	
+	std::fstream tryFile;
+	// CHECK FOR CORRECT NUMBER OF ARGUMENTS
 	if (argc != 4)
 	{
 		std::cout << "Correct usage: ./find_and_replace <filename> <string to replace> <replacement string>" << std::endl;
 		std::exit(1);
 	}
+	// CHECK IF PROVIDED FILE IS READABLE
+	tryFile.open(argv[1], std::ios::in);
+	if (!tryFile)
+	{
+		std::cout << "Error: File " << argv[1] << " cannot be opened for reading." << std::endl;
+		std::exit(1);
+	}
+	tryFile.close();
 	
+	return (0);
 }
 
 // std::find() to find the string?, erase() in the range, then insert()
@@ -33,8 +42,13 @@ int main(int argc, char **argv)
 	std::string file;
 	std::string s1;
 	std::string s2;
+	std::string line;
 
 	inputParser(argc, argv);
+	file = argv[1];
+	s1 = argv[2];
+	s2 = argv[3];
+
 	
 
 	return (0);
