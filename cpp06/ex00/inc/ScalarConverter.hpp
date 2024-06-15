0085be154831c8dd05768e3203de79c82eef3c2f
sl16 <vojtabar119@gmail.com>
2024-06-15 16:56:07 +0200

    cpp06 ex00 progress

--- cpp06/ex00/inc/ScalarConverter.hpp
+++ cpp06/ex00/inc/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: vbartos <vbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 21:49:59 by vbartos           #+#    #+#             */
-/*   Updated: 2024/06/13 13:18:27 by vbartos          ###   ########.fr       */
+/*   Updated: 2024/06/15 15:57:15 by vbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 #include <string>
 #include <iostream>
+#include <iomanip>
 #include <cstdlib>
+#include <limits>
 
 #define SYMBOLS_INT	"-0123456789"
 #define SYMBOLS_FLT	"-0123456789f."
--- cpp06/ex00/src/ScalarConverter.cpp
+++ cpp06/ex00/src/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: vbartos <vbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 21:49:51 by vbartos           #+#    #+#             */
-/*   Updated: 2024/06/13 13:30:42 by vbartos          ###   ########.fr       */
+/*   Updated: 2024/06/15 15:55:39 by vbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -124,6 +124,54 @@ static ScalarValue convertToOriginal(std::string value)
 	return (res);
 }
 
+static void printChar(ScalarValue valueOg)
+{
+	std::cout << "char: \t";
+	if (valueOg.value.c < 32 || valueOg.value.c > 126)
+		std::cout << "non displayable" << std::endl;
+	else
+		std::cout << valueOg.value.c << std::endl;
+	std::cout << "int: \t" << static_cast<int>(valueOg.value.c) << std::endl;
+	std::cout << "float: \t" << std::fixed << std::setprecision(1) << static_cast<float>(valueOg.value.c) << "f" << std::endl;
+	std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(valueOg.value.c) << std::endl;
+}
+
+static void printInt(ScalarValue valueOg)
+{
+	std::cout << "char: \t";
+	if (valueOg.value.i < 32 || valueOg.value.i > 126)
+		std::cout << "non displayable" << std::endl;
+	else
+		std::cout << static_cast<char>(valueOg.value.i) << std::endl;
+	std::cout << "int: \t" << valueOg.value.i << std::endl;
+	std::cout << "float: \t" << std::fixed << std::setprecision(1) << static_cast<float>(valueOg.value.i) << "f" << std::endl;
+	std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(valueOg.value.i) << std::endl;
+}
+
+static void printFloat(ScalarValue valueOg)
+{
+	std::cout << "char: \t";
+	if (valueOg.value.f < 32 || valueOg.value.f > 126)
+		std::cout << "non displayable" << std::endl;
+	else
+		std::cout << static_cast<char>(valueOg.value.f) << std::endl;
+	std::cout << "int: \t" << static_cast<int>(valueOg.value.f) << std::endl;
+	std::cout << "float: \t" << std::fixed << std::setprecision(1) << valueOg.value.f << "f" << std::endl;
+	std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(valueOg.value.f) << std::endl;
+}
+
+static void printDouble(ScalarValue valueOg)
+{
+	std::cout << "char: \t";
+	if (valueOg.value.d < 32 || valueOg.value.d > 126)
+		std::cout << "non displayable" << std::endl;
+	else
+		std::cout << static_cast<char>(valueOg.value.d) << std::endl;
+	std::cout << "int: \t" << static_cast<int>(valueOg.value.d) << std::endl;
+	std::cout << "float: \t" << std::fixed << std::setprecision(1) << static_cast<float>(valueOg.value.d) << "f" << std::endl;
+	std::cout << "double: " << std::fixed << std::setprecision(1) << valueOg.value.d << std::endl;
+}
+
 // MEMBER FUNCTIONS
 
 void ScalarConverter::convert(std::string value)
@@ -132,14 +180,20 @@ void ScalarConverter::convert(std::string value)
 	
 	valueOg = convertToOriginal(value);
 
-	std::cout << valueOg.value.c << std::endl;
-	std::cout << valueOg.value.i << std::endl;
-	std::cout << valueOg.value.f << std::endl;
-	std::cout << valueOg.value.d << std::endl;
-
-	std::cout << "char: \t" << static_cast<char>(valueOg.value.c) << std::endl;
-	std::cout << "int: \t" << static_cast<int>(valueOg.value.i) << std::endl;
-	std::cout << "float: \t" << static_cast<float>(valueOg.value.f) << std::endl;
-	std::cout << "double: \t" << static_cast<double>(valueOg.value.d)<< std::endl;
+	switch (valueOg.type)
+	{
+		case IS_CHAR:
+			printChar(valueOg);
+			break;
+		case IS_INT:
+			printInt(valueOg);
+			break;
+		case IS_FLOAT:
+			printFloat(valueOg);
+			break;
+		case IS_DOUBLE:
+			printDouble(valueOg);
+			break;
+	}
 
 }
