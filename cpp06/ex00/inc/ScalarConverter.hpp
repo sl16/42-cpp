a65c120248d836f7d6ccbd24a3052f753a73fb1b
sl16 <vojtabar119@gmail.com>
2024-06-17 14:27:41 +0200

    cpp06 ex00 almost there :)

--- cpp06/ex00/inc/ScalarConverter.hpp
+++ cpp06/ex00/inc/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: vbartos <vbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 21:49:59 by vbartos           #+#    #+#             */
-/*   Updated: 2024/06/17 12:00:53 by vbartos          ###   ########.fr       */
+/*   Updated: 2024/06/17 14:20:26 by vbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,8 @@
 #define SYMBOLS_FLT	"-0123456789f."
 #define SYMBOLS_DBL	"-0123456789."
 
-// #define IS_CHAR		0
-// #define IS_INT		1
-// #define IS_FLOAT	2
-// #define IS_DOUBLE	3
+#define IMPOSSIBLE	"impossible"
+#define NON_DISPL	"non-displayable"
 
 class ScalarConverter
 {
@@ -45,7 +43,10 @@ enum ValueType
     IS_CHAR,
     IS_INT,
     IS_FLOAT,
-    IS_DOUBLE
+    IS_DOUBLE,
+	IS_NAN,
+	IS_INF_POS,
+	IS_INF_NEG
 };
 
 // union ValueUnion
@@ -56,13 +57,13 @@ enum ValueType
 //     double d;
 // };
 
-struct ScalarValue
-{
-    ValueType 		type;
-    unsigned char	c;
-	int				i;
-	float			f;
-	double			d;
-	bool		valid = true;
-	bool		displayable = true;
-};
+// struct ScalarValue
+// {
+//     ValueType 		type;
+//     unsigned char	c;
+// 	int				i;
+// 	float			f;
+// 	double			d;
+// 	bool		valid = true;
+// 	bool		displayable = true;
+// };
--- cpp06/ex00/src/ScalarConverter.cpp
+++ cpp06/ex00/src/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: vbartos <vbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 21:49:51 by vbartos           #+#    #+#             */
-/*   Updated: 2024/06/17 12:57:46 by vbartos          ###   ########.fr       */
+/*   Updated: 2024/06/17 14:27:24 by vbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,194 +72,209 @@ static bool isDouble(std::string value)
 		return (false);
 }
 
+static bool isNan(std::string value)
+{
+	if (value == "nanf" || value == "nan")
+		return (true);
+	else
+		return (false);
+}
+
+static bool isInfPos(std::string value)
+{
+	if (value == "+inff" || value == "+inf")
+		return (true);
+	else
+		return (false);
+}
+
+static bool isInfNeg(std::string value)
+{
+	if (value == "-inff" || value == "-inf")
+		return (true);
+	else
+		return (false);
+}
+
 static char		stringToChar(std::string value)
 {
 	return (static_cast<char>(value[0]));
 }
 
-static int 		stringToInt(std::string value, ScalarValue data)
+static long 	stringToInt(std::string value)
 {
-    std::istringstream iss(value);
-    long long intVal;
-
-    if (!(iss >> intVal) || intVal < std::numeric_limits<int>::min() || intVal > std::numeric_limits<int>::max())
-		data.valid = false;
-    return (atoi(value.c_str()));
+    return (atol(value.c_str()));
 }
 
 static float	stringToFloat(std::string value)
 {
-	return (atof(value.c_str()));
+	return (std::strtof(value.c_str(), NULL));
 }
 
 static double	stringToDouble(std::string value)
 {
-	return (atof(value.c_str()));
+	return (std::strtod(value.c_str(), NULL));
 }
 
-static ScalarValue convertToOriginal(std::string value)
-{
-	ScalarValue res;
-	
+ValueType getType(std::string value)
+{	
 	if (isChar(value))
-	{
-		res.type = IS_CHAR;
-		res.c = stringToChar(value);
-		std::cout << "ogType: char" << std::endl << std::endl;
-	}
+		return (IS_CHAR);
 	else if (isInt(value))
-	{
-		res.type = IS_INT;
-		res.i = stringToInt(value, res);
-		std::cout << "ogType: int" << std::endl << std::endl;
-	}
+		return (IS_INT);
 	else if (isFloat(value))
-	{
-		res.type = IS_FLOAT;
-		res.f = stringToFloat(value);
-		std::cout << "ogType: float" << std::endl << std::endl;
-	}
+		return (IS_FLOAT);
 	else if (isDouble(value))
-	{
-		res.type = IS_DOUBLE;
-		res.d = stringToDouble(value);
-		std::cout << "ogType: double" << std::endl << std::endl;
-	}
+		return (IS_DOUBLE);
+	else if (isNan(value))
+		return (IS_NAN);
+	else if (isInfNeg(value))
+		return (IS_INF_NEG);
+	else if (isInfPos(value))
+		return (IS_INF_POS);
 	else
 	{
 		std::cout << "I don't know how to convert that. Are you crazy?!" << std::endl;
 		exit(1);
 	}
-	return (res);
 }
 
-static void	typeToChar(ScalarValue ogValue, ScalarValue &res)
-{
-	if (ogValue.type == IS_CHAR)
-	{
-		res.c = ogValue.c;
-		return ;
-	}
-	
-
-}
-
-static void	typeToInt(ScalarValue ogValue, ScalarValue &res)
-{
-	if (ogValue.type == IS_INT)
-	{
-		res.i = ogValue.i;
-		return ;
-	}
-}
-
-static void	typeToFloat(ScalarValue ogValue, ScalarValue &res)
-{
-	if (ogValue.type == IS_FLOAT)
-	{
-		res.f = ogValue.f;
-		return ;
-	}
-}
-
-static void	typeToDouble(ScalarValue ogValue, ScalarValue &res)
-{
-	if (ogValue.type == IS_DOUBLE)
-	{
-		res.d = ogValue.d;
-		return ;
-	}
-}
-
-static ScalarValue convertFromOriginal(ScalarValue &ogValue, ValueType convertToType)
-{
-	ScalarValue res;
-	
-	if (convertToType == IS_CHAR)
-	{
-		res.type = IS_CHAR;
-		typeToChar(ogValue, res);
-	}
-	else if (convertToType == IS_INT)
-	{
-		res.type = IS_INT;
-		typeToInt(ogValue, res);
-	}
-	else if (convertToType == IS_FLOAT)
-	{
-		res.type = IS_FLOAT;
-		typeToFloat(ogValue, res);
-	}
-	else if (convertToType == IS_DOUBLE)
-	{
-		res.type = IS_DOUBLE;
-		typeToDouble(ogValue, res);
-	}
-	return (res);
-}
-
-static void printValues(ScalarValue valueC, ScalarValue valueI, ScalarValue valueF, ScalarValue valueD)
+void printFromChar(unsigned char c)
 {
 	std::cout << "char: \t";
-	if (valueC.valid == false)
-		std::cout << "impossible" << std::endl;
-	else if (valueC.displayable == false)
-		std::cout << "non displayable" << std::endl;
+	if (c < 32 || c > 126)
+		std::cout << NON_DISPL << std::endl;
 	else
-		std::cout << valueC.c << std::endl;
+		std::cout << c << std::endl;
+	
+	std::cout << "int: \t" << static_cast<int>(c) << std::endl;
 
-	std::cout << "int: \t";
-	if (valueI.valid == false)
-		std::cout << "impossible" << std::endl;
-	else
-		std::cout << valueI.i << std::endl;
+	std::cout << "float: \t" << std::fixed << std::setprecision(1) << static_cast<float>(c) << "f" << std::endl;
+	
+	std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(c) << std::endl;
+}
 
-	std::cout << "float: \t";
-	if (valueF.valid == false)
-		std::cout << "impossible" << std::endl;
+void printFromInt(long i)
+{
+	std::cout << "char: \t";
+	if (i < std::numeric_limits<char>::min() || i > std::numeric_limits<char>::max())
+		std::cout << IMPOSSIBLE << std::endl;
+	else if (i < 32 || i > 126)
+		std::cout << NON_DISPL << std::endl;
 	else
-		std::cout << valueF.f << std::endl;
+		std::cout << static_cast<char>(i) << std::endl;
 
-	std::cout << "double: \t";
-	if (valueD.valid == false)
-		std::cout << "impossible" << std::endl;
+	std::cout << "int: \t" << std::endl;
+	if (i < std::numeric_limits<int>::min() || i > std::numeric_limits<int>::max())
+		std::cout << IMPOSSIBLE << std::endl;
 	else
-		std::cout << valueD.d << std::endl;
+		std::cout << i;
+		
+	std::cout << "float: \t" << std::fixed << std::setprecision(1) << static_cast<float>(i) << "f" << std::endl;
+	
+	std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(i) << std::endl;
+}
+
+void printFromFloat(float f)
+{
+	std::cout << "char: \t";
+	if (f < std::numeric_limits<char>::min() || f > std::numeric_limits<char>::max())
+		std::cout << IMPOSSIBLE << std::endl;
+	else if (f < 32 || f > 126)
+		std::cout << NON_DISPL << std::endl;
+	else
+		std::cout << static_cast<char>(f) << std::endl;
+	
+	if (f < std::numeric_limits<int>::min() || f > std::numeric_limits<int>::max())
+		std::cout << IMPOSSIBLE << std::endl;
+	else
+		std::cout << f;
+
+	std::cout << "float: \t" << std::fixed << std::setprecision(1) << f << "f" << std::endl;
+	
+	std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(f) << std::endl;
+}
+
+void printFromDouble(double d)
+{
+	std::cout << "char: \t";
+	if (d < 32 || d > 126)
+		std::cout << NON_DISPL << std::endl;
+	else
+		std::cout << static_cast<char>(d) << std::endl;
+	
+	if (d < std::numeric_limits<int>::min() || d > std::numeric_limits<int>::max())
+		std::cout << IMPOSSIBLE << std::endl;
+	else
+		std::cout << d;
+
+	std::cout << "float: \t" << std::fixed << std::setprecision(1) << static_cast<float>(d) << "f" << std::endl;
+	
+	std::cout << "double: " << std::fixed << std::setprecision(1) << d << std::endl;
+}
+
+void printPseudo(std::string str_value, ValueType ogType)
+{
+	float			f;
+	double			d;
+	
+	if (ogType == IS_INF_POS)
+	{
+		f = static_cast<float>(std::numeric_limits<float>::infinity());
+		d = static_cast<double>(std::numeric_limits<double>::infinity());
+	}
+	else if (ogType == IS_INF_NEG)
+	{
+		f = static_cast<float>(std::numeric_limits<float>::infinity() * -1.f);
+		d = static_cast<double>(std::numeric_limits<double>::infinity() * -1.);
+	}
+	else if (ogType == IS_NAN)
+	{
+		f = static_cast<float>(std::numeric_limits<float>::quiet_NaN());
+		d = static_cast<double>(std::numeric_limits<double>::quiet_NaN());
+	}
+	std::cout << "char:\t" << IMPOSSIBLE << std::endl;
+	std::cout << "int:\t" << IMPOSSIBLE << std::endl;
+	std::cout << "float: " << f << "f" << std::endl;
+	std::cout << "double: " << d << std::endl;
 }
 
 // MEMBER FUNCTIONS
 
-void ScalarConverter::convert(std::string value)
+void ScalarConverter::convert(std::string str_value)
 {
-	ScalarValue valueOg;
-	ScalarValue valueC;
-	ScalarValue valueI;
-	ScalarValue valueF;
-	ScalarValue valueD;
-	
-	valueOg = convertToOriginal(value);
-	valueC = convertFromOriginal(valueOg, IS_CHAR);
-	valueI = convertFromOriginal(valueOg, IS_INT);
-	valueF = convertFromOriginal(valueOg, IS_FLOAT);
-	valueD = convertFromOriginal(valueOg, IS_DOUBLE);
+	unsigned char	c;
+	long			i;
+	float			f;
+	double			d;
+		
+	ValueType ogType = getType(str_value);
 
-	printValues(valueC, valueI, valueF, valueD);
-	
-
-	// switch (valueOg.type)
-	// {
-	// 	case IS_CHAR:
-	// 		printChar(valueOg);
-	// 		break;
-	// 	case IS_INT:
-	// 		printInt(valueOg);
-	// 		break;
-	// 	case IS_FLOAT:
-	// 		printFloat(valueOg);
-	// 		break;
-	// 	case IS_DOUBLE:
-	// 		printDouble(valueOg);
-	// 		break;
-	// }
+	switch(ogType)
+	{
+	    case IS_CHAR:
+			c = stringToChar(str_value);
+			printFromChar(c);
+	        break;
+	    case IS_INT:
+	        i = stringToInt(str_value);
+			printFromInt(i);
+	        break;
+	    case IS_FLOAT:
+	        f = stringToFloat(str_value);
+			printFromFloat(f);
+	        break;
+	    case IS_DOUBLE:
+	        d = stringToDouble(str_value);
+			printFromDouble(d);
+	        break;
+		case IS_NAN:
+		case IS_INF_NEG:
+		case IS_INF_POS:
+			printPseudo(str_value, ogType);
+			break;
+	    default:
+			std::cout << "I don't know how to convert that. Are you crazy?!" << std::endl;
+	}
 
 }
